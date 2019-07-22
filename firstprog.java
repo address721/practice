6998ad8623d8414557f98668a9179f7f88376cff
address721 <ggmm0699@gmail.com>
2019-07-22 07:30:19 +0530

    first commit

---
+++ firstprog.java
@@ -0,0 +1,22 @@
+import java.io.*
+import java.util.*
+
+public class firstprogram
+{
+	public static void main(String[] args)
+	{
+		int a , b;
+		int c = max(a,b);
+	}
+
+	int max(int a, int b)
+	{
+		if(a>b)
+		{
+			return a;
+		}
+		return b;
+	
+	}
+}
+
